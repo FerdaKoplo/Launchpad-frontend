66be3e3c0cb8aeb07c7270298b18616f606c0d27
FerdaKoplo <sahmuraivan43@gmail.com>
2025-10-04 02:16:13 +0700

    slicing auth

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,22 +1,35 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { IoMdHome } from "react-icons/io";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex  items-center justify-center bg-gradient-to-b from-emerald-400 via-teal-500 to-cyan-500 relative overflow-hidden">
+    <div className="min-h-screen flex flex-col  items-center justify-center bg-gradient-to-b from-emerald-400 via-teal-500 to-cyan-500 relative overflow-hidden gap-5">
 
-      <div className="flex justify-between  items-start">
-        <h1>
-          Launchpad
+      <div className="flex flex-col justify-center gap-2  items-center group">
+        <h1 className="text-5xl hover:scale-110 group-hover:[&:not(:hover)]:scale-90  duration-300 font-bold border-white/20 border-2 bg-gradient-to-tl
+          from-emerald-300 ease-in-out via-teal-500 to-emerald-300 px-4 py-2 rounded-xl text-white">
+          LAUNCHPAD
         </h1>
-        <Link href={''} >
-          <p>
-            Back to main page
-          </p>
+        <Link href={'/'}>
+          <Button className=" flex items-center gap-2 hover:scale-110 group-hover:[&:not(:hover)]:scale-90  
+            duration-300 text-white bg-gradient-to-tr from-teal-300 px-2 py-1 rounded-lg border-2 border-white/40 via-teal-500 to-emerald-300 font-semibold text-lg"          >
+            <IoMdHome />
+            Back to Main Page
+          </Button>
         </Link>
       </div>
 
-      <div className="bg-white/90 backdrop-blur-md p-8 rounded-2xl shadow-lg w-full max-w-md">
+      <div className="relative bg-white/20 backdrop-blur-xl border-2 border-white/40 p-8  rounded-t-2xl rounded-b-[5rem] shadow-lg w-full max-w-xl">
         {children}
+        <>
+          <div className="rounded-full bg-white/10 border-white/30 border-r-4 absolute w-20 h-20 top-2   -left-10" />
+          <div className="rounded-full bg-white/15 border-white/30 border-r-4 absolute w-7 h-7 top-[7rem]   left-4" />
+        </>
+        <>
+          <div className="rounded-full bg-white/15 border-white/30 border-r-4 absolute w-7 h-7 bottom-[7rem]   right-4" />
+          <div className="rounded-full bg-white/10 border-white/30 border-r-4 absolute w-10 h-10 bottom-[10rem]   -right-7" />
+        </>
       </div>
     </div>
   )
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,9 +3,13 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 
-const Login = () => {
+const page = () => {
   return (
-    <form className="flex flex-col gap-10">
+    <form action="" className="flex flex-col gap-10">
+      <div className="flex text-white flex-col justify-center gap-4">
+        <Label className="font-bold">Name</Label>
+        <Input placeholder="Enter your name..." className="placeholder:text-white focus-visible:ring-white focus:ring-white border-white focus-visible:ring-0 focus-visible:ring-offset-0" />
+      </div>
       <div className="flex text-white flex-col justify-center gap-4">
         <Label className="font-bold">Email</Label>
         <Input placeholder="Enter your email..." className="placeholder:text-white focus-visible:ring-white focus:ring-white border-white focus-visible:ring-0 focus-visible:ring-offset-0" />
@@ -14,16 +18,20 @@ const Login = () => {
         <Label className="text-white font-bold">Password</Label>
         <Input placeholder="Enter your password..." className="placeholder:text-white text-white border-white focus-visible:ring-0 focus-visible:ring-offset-0 transition-none" />
       </div>
+      <div className="flex flex-col gap-4">
+        <Label className="text-white font-bold">Confirm Password</Label>
+        <Input placeholder="Enter your password..." className="placeholder:text-white text-white border-white focus-visible:ring-0 focus-visible:ring-offset-0 transition-none" />
+      </div>
       <Button type="submit" className="hover:scale-110  bg-white/20 shadow-md border-white/50 hover:bg-white/20 border-t-2 text-white font-bold">
-        Login
+        Register
       </Button>
-      <Link href={'/auth/register'}>
+      <Link href={'/auth/login'}>
         <p className="flex justify-center text-white">
-          Register here if dont have account yet!
+          Login here if you already had account!
         </p>
       </Link>
     </form>
   )
 }
 
-export default Login
+export default page
